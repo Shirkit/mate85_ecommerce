1a4e0a2274e6755a681926396ee9f11392330edc
Danilo Andrade <andrade20danilo@gmail.com>
2023-09-03 11:16:03 -0300

    added navbar and footer to layout

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
-
 export default function Navbar() {
   return (
-    <div>
-      <h1>SHOP.CO</h1>
-      <Link href = "/">Shop</Link>
-      <Link href = "/">Em Promoção</Link>
-      <Link href = "/">Novos produtos</Link>
-      <Link href= "/">Marcas</Link>
+    <div className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-white text-2xl font-semibold">SHOP.CO</h1>
+        <nav className="space-x-4">
+          <Link href="/">Shop</Link>
+          <Link href="/">Em Promoção</Link>
+          <Link href="/">Novos produtos</Link>
+          <Link href="/">Marcas</Link>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
