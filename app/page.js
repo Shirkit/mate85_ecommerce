f25ffe87644092c9900974aff1f36434db5ce279
Danilo Andrade <andrade20danilo@gmail.com>
2023-09-03 17:12:45 -0300

    comecei o layout basico da landing page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,42 @@
 import Image from 'next/image'
+import Card from '@/components/ui/Card';
+import { prisma } from "@/utils/prisma"
 
 
-const products = [
+
+
+export default async function Home() {
+  const products = [
   {"id":1,"name":"seila","price":1},
   {"id":2,"name":"seila2","price":2},
   {"id":3,"name":"seila3","price":3},
-]
-export default function Home() {
+  ]
+
+  // const products = await prisma.product.findMany();
+
   return (
     <main>
-      <h1>Home</h1>
+
+      <div className="min-h-screen flex flex-col items-center justify-start py-8">
+      <div className="text-center font-bold text-xl mb-4">NOVOS PRODUTOS</div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.map( (product) => {
+            return (
+              <Card 
+              key = {product.id} 
+              name = {product.name} 
+              image = {`https://picsum.photos/id/${Math.round(Math.random() * 1084)}/200`} 
+              price = {product.price}
+              rating = {3}
+              />
+              
+            )
+          })}
+        </div>
+  
+      </div>
+
     </main>
   )
 }
