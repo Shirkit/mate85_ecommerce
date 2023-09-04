e259bdcf02fd0cf20af86b9e7a516f1bcd047897
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-09-04 19:11:35 -0300

    feat: finalize header

--- app/page.js
+++ app/page.js
@@ -1,43 +1,48 @@
 import Image from 'next/image'
-import Card from '@/components/ui/Card';
-import { prisma } from "@/utils/prisma"
+import Card from '@/components/ui/Card'
+import { prisma } from '@/utils/prisma'
+import { Candal } from 'next/font/google'
 
-
-
+const candal = Candal({
+	weight: ['400'],
+	subsets: ['latin'],
+})
 
 export default async function Home() {
-  const products = [
-  {"id":1,"name":"seila","price":1},
-  {"id":2,"name":"seila2","price":2},
-  {"id":3,"name":"seila3","price":3},
-  {"id":4,"name":"seila4","price":5},
-  ]
+	const products = [
+		{ id: 1, name: 'seila', price: 1 },
+		{ id: 2, name: 'seila2', price: 2 },
+		{ id: 3, name: 'seila3', price: 3 },
+		{ id: 4, name: 'seila4', price: 5 },
+	]
 
-  // const products = await prisma.product.findMany();
+	// const products = await prisma.product.findMany();
 
-  return (
-    <main>
+	return (
+		<main>
+			<div
+				className={`${candal.className} min-h-screen flex flex-col items-center justify-start py-8`}
+			>
+				<div className="text-center font-bold text-xl mb-4">
+					NOVOS PRODUTOS
+				</div>
 
-      <div className="min-h-screen flex flex-col items-center justify-start py-8">
-      <div className="text-center font-bold text-xl mb-4">NOVOS PRODUTOS</div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {products.map( (product) => {
-            return (
-              <Card 
-              key = {product.id} 
-              name = {product.name} 
-              image = {`https://picsum.photos/id/${Math.round(Math.random() * 1084)}/200`} 
-              price = {product.price}
-              rating = {3}
-              />
-              
-            )
-          })}
-        </div>
-  
-      </div>
-
-    </main>
-  )
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+					{products.map((product) => {
+						return (
+							<Card
+								key={product.id}
+								name={product.name}
+								image={`https://picsum.photos/id/${Math.round(
+									Math.random() * 1084
+								)}/200`}
+								price={product.price}
+								rating={3}
+							/>
+						)
+					})}
+				</div>
+			</div>
+		</main>
+	)
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,36 +1,44 @@
-import Link from "next/link";
-import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { Search, ShoppingCart, UserCircle2 } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Navbar() {
-  return (
-    <div className="bg-blue-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-semibold">SHOP.CO</h1>
-        <div className="flex items-center space-x-4">
-          <nav className="space-x-4">
-            <Link href="/">Shop</Link>
-            <Link href="/">Em Promoção</Link>
-            <Link href="/">Novos produtos</Link>
-            <Link href="/">Marcas</Link>
-          </nav>
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Busque por Produto"
-              className="border rounded-lg px-3 py-1 focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <FaSearch className="text-gray-400" />
-            </div>
-          </div>
-          <div>
-            <FaShoppingCart className="text-white text-xl cursor-pointer" />
-          </div>
-          <div>
-            <FaUser className="text-white text-xl cursor-pointer" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<header className="p-4">
+			<div className="container mx-auto flex justify-between items-center gap-10 max-w-7xl text-zinc-900 ">
+				<h1 className="font-primary font-black text-3xl">SHOP.CO</h1>
+				<nav className="flex flex-row gap-6">
+					<Link href="/" className="w-max">
+						Shop
+					</Link>
+					<Link href="/" className="w-max">
+						Em Promoção
+					</Link>
+					<Link href="/" className="w-max">
+						Novos produtos
+					</Link>
+					<Link href="/" className="w-max">
+						Marcas
+					</Link>
+				</nav>
+				<div className="relative w-full">
+					<input
+						type="text"
+						placeholder="Busque por produto"
+						className="w-full border rounded-full px-12 py-3 bg-zinc-200 focus:outline-none focus:ring focus:border-blue-300"
+					/>
+					<div className="absolute inset-y-0 left-4 flex items-center">
+						<Search className="text-gray-400" />
+					</div>
+				</div>
+				<div className="flex items-center gap-4">
+					<div>
+						<ShoppingCart className="text-xl cursor-pointer" />
+					</div>
+					<div>
+						<UserCircle2 className="text-xl cursor-pointer" />
+					</div>
+				</div>
+			</div>
+		</header>
+	)
 }
