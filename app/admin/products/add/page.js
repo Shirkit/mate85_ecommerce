842a93dc930e5a01ae18177f964b362cae603dc0
Shirkit <Shirkit@users.noreply.github.com>
2023-09-07 14:22:54 -0300

    Criado estrutura para features futuras do admin

---
+++ app/admin/products/add/page.js
@@ -0,0 +1,7 @@
+
+export default function AddEditProduct() {
+
+    return (
+        <div></div>
+    )
+}
---
+++ app/admin/users/add/page.js
@@ -0,0 +1,7 @@
+
+export default function AddEditUser() {
+
+    return (
+        <div></div>
+    )
+}
