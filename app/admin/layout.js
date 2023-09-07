8bb7fdd6916d0844b3c316229f268eebd0b33b4d
Shirkit <Shirkit@users.noreply.github.com>
2023-09-07 14:22:38 -0300

    Adicionado layout básico para parte administrativa

---
+++ app/admin/layout.js
@@ -0,0 +1,28 @@
+import AdminNavbar from '@/components/admin/navbar'
+import './admin.css'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+	title: 'Shop.CO',
+	description: 'descricao',
+}
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<header className="items-center text-zinc-100 mx-auto flex justify-between w-full bg-zinc-900 p-1">
+					<Link href="/admin" className="w-max">
+						<h1 className="font-primary font-black text-3xl">SHOP.CO</h1>
+					</Link>
+				</header>
+				<div className="flex flex-row">
+					<AdminNavbar></AdminNavbar>
+					{children}
+				</div>
+			</body>
+		</html>
+	)
+}
