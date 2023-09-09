49eacd6ef7c1052c56ea907887f375016e6eb9f2
Queila Abreu <queilahabreu@gmail.com>
2023-09-08 22:32:47 -0300

    fix: corrigindo bug import footer

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -1,7 +1,7 @@
 import './website.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
-import Footer from '@/components/Footer'
+import Footer from '@/components/footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
