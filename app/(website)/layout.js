fb083a3aae4f16815e59ed4164e380d6c7d85bf4
Shirkit <Shirkit@users.noreply.github.com>
2023-09-13 11:47:49 -0300

    feat (packages): adicionado pacote para breadcrumbs e carousel

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -2,6 +2,8 @@ import './website.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
+import Breadcrumbs from "@marketsystems/nextjs13-appdir-breadcrumbs"
+import "@marketsystems/nextjs13-appdir-breadcrumbs/dist/styles.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 				<Navbar />
+				<Breadcrumbs useDefaultStyle rootLabel="Home" />
 				{children}
 				<Footer />
 			</body>
