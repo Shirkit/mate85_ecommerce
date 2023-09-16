c198140d2cb7b6e3513af6042f0e41e434305650
Shirkit <Shirkit@users.noreply.github.com>
2023-09-16 00:26:06 -0300

    feat (see-product-client): removid os breadscrumbs temporários

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -2,8 +2,6 @@ import './website.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
-import Breadcrumbs from "@marketsystems/nextjs13-appdir-breadcrumbs"
-import "@marketsystems/nextjs13-appdir-breadcrumbs/dist/styles.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -16,7 +14,6 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 				<Navbar />
-				<Breadcrumbs useDefaultStyle rootLabel="Home" />
 				{children}
 				<Footer />
 			</body>
