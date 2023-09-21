94401e5bbb6489b741613e5e88a20e4bcfc8d688
Shirkit <Shirkit@users.noreply.github.com>
2023-09-21 01:37:18 -0300

    feat: :sparkles: Adicionada biblioteca react-toastify
    Para responder com mensagens toast após servidor processar

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -2,6 +2,8 @@ import './website.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 				<Navbar />
+				<ToastContainer></ToastContainer>
 				{children}
 				<Footer />
 			</body>
