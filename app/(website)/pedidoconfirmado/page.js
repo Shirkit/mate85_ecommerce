d80ff2b6ca61119e7716afaab59c0b2111c563dd
Julian Reina <julianreina@gmail.com>
2023-09-21 21:12:40 -0300

    test: add param atraves da order_id

--- app/(website)/pedidoconfirmado/page.js
+++ app/(website)/pedidoconfirmado/page.js
@@ -1,21 +1,20 @@
+
 import React from 'react';
 
 import CardConfirmacao from '@/components/pedidoconfirmado/CardConfirmacao';
 
 
-export default async function Home() {
-	let products = [
-		{ id: 1, name: 'seila', price: 1 },
-		{ id: 2, name: 'seila2', price: 2 },
-		{ id: 3, name: 'seila3', price: 3 },
-		{ id: 4, name: 'seila4', price: 5 },
-	]
+export default async function PedidoConfirmado(params) {
+
+
+	const pedido = await prisma.order.findFirst({ where: { id: params.id }, include: {user: true }});
 
 	
 
 	return (
 		<main>
 			{
-                <CardConfirmacao></CardConfirmacao>
+                <CardConfirmacao pedido={pedido}></CardConfirmacao>
                 }
                 </main>)}
+				
--- components/pedidoconfirmado/CardConfirmacao.jsx
+++ components/pedidoconfirmado/CardConfirmacao.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 
 
-export default function CardConfirmacao({
-    nome,
-    email,
-    total,
-    cepEntrega,
-    formaPagamento,
-    
- }) {  
+export default function CardConfirmacao(props) {
+    const pedido = props.pedido;
+    const produtos = pedido.order_items;
+    const user = pedido.user;  
     return ( 
     
     
@@ -20,8 +16,8 @@ export default function CardConfirmacao({
 
         <div className='bg-green-100 p-4 h-auto md:h-2/6 border border-green-400 w-auto md:w-2/4 rounded-sm'>
 <strong><p className='text-green-800'>Seu pedido foi realizado com sucesso.</p></strong>
-<p className='text-green-700'>Obrigado por comprar conosco <strong>Julian Reina</strong>. Em breve você receberá um email no 
-endereço<strong> teste@hotmail.com </strong>  
+<p className='text-green-700'>Obrigado por comprar conosco <strong>{user.name}</strong>. Em breve você receberá um email no 
+endereço<strong> {user.email} </strong>  
  com todos os detalhes do pedido
 </p>
 <div className='flex items-center bg-green-500 h-auto md:h-2/6 border border-green-400 w-auto md:w-full mt-8'>
@@ -35,16 +31,51 @@ endereço<strong> teste@hotmail.com </strong>
 
         <div className=' p-4 h-auto md:h-2/6 border border-black w-auto md:w-2/4 rounded-sm '>
         <h4 className='font-bold'>Informações Do Pedido</h4>
-        <p> ID: 1421</p>
-        <p> Total: R$ 235,40</p>
-        <p>Cep de entrega: 41810-105</p>
-        <p>Forma de envio: Correios PAC</p>
+        <p> Numero do pedido: {pedido.order_number}</p>
+        <div className='flex justify-center bg-black text-white'>Items</div>
+        <table className='border-collapse'>
+        <thead>
+            
+        <tr>
+          <th>Produto</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
 
+        {/* {produtos.forEach(produto => {
+            <tr key={produto.id}>
+            <td>{produto.produto.nome} x 1</td> 
+            <td>R${produto.produto.preco}</td>
+          </tr>
+    
+        })} */}
+
+        {/*Adicionar estilização de tabela*/}
+        <tr>
+            <td>azul</td>
+            <td>quente</td>
+        </tr>
+        <tr>
+            <td>azul x1</td>
+            <td>quente </td>
+        </tr>
+        <tr>
+            <td>azul</td>
+            <td>quente</td>
+        </tr>
+        
+        <p> Total: R${pedido.total}</p>
+        </tbody>
+        </table>
         <br>
         </br>
         <p>*Os produtos serão enviados em até 2 dias após a confirmação do pagamento.</p>
         </div>
-
+        <div className=' p-4 h-auto md:h-2/6 border border-black w-auto md:w-2/4 rounded-sm '>
+        <h4 className='font-bold'>Informações de Cobrança</h4>
+        {/*Adicionar campos de endereço*/}
+        </div>
 
     </div>
     
