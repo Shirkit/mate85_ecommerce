7bcb2879815acf70b92fc38b72034b9c5f5a7648
JulianReina96 <julianreina@gmail.com>
2023-09-22 20:02:03 -0300

    test: add dados bd na view renderizada

--- app/(website)/pedidoconfirmado/page.js
+++ app/(website)/pedidoconfirmado/page.js
@@ -7,7 +7,11 @@ import CardConfirmacao from '@/components/pedidoconfirmado/CardConfirmacao';
 export default async function PedidoConfirmado(params) {
 
 
-	const pedido = await prisma.order.findFirst({ where: { id: params.id }, include: {user: true }});
+	const pedido = await prisma.order.findFirst({ where: { id: params.id }, include: {user: true, address:true, order_items: {
+		include: {
+		  product: true, 
+		},
+	  },},});
 
 	
 
