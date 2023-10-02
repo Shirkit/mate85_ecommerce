c58e7b393b01705a047f7bd7759f391cb7b27e45
Shirkit <Shirkit@users.noreply.github.com>
2023-10-02 12:16:50 -0300

    refactor(cart): :lipstick: ajeitado layout para grid

--- components/ui/cartItem.jsx
+++ components/ui/cartItem.jsx
@@ -34,18 +34,23 @@ export const CartItem = ({ item }) => {
 
   return (
     <Fragment key={item.product.sku}>
-      <div className=""><Image width={100} height={100} src={`https://picsum.photos/id/${item.product.id}/100`}></Image></div>
-      <div className="flex flex-col">{item.product.name}
-        <span className="text-sm">{item.product.sku}</span>
-        <div><span className="text-xs text-red-400 cursor-pointer" onClick={handleRemoveClick}>Remover</span></div>
+      <div className="flex justify-center"><Image width={100} height={100} src={`https://picsum.photos/id/${item.product.id}/100`}></Image></div>
+      <div className="flex justify-center items-center">
+        <div>
+          <div className="inline-block">{item.product.name}</div><br/>
+          <div className="inline-block text-xs">{item.product.sku}</div><br/>
+          <div className="inline-block text-xs text-red-400 cursor-pointer" onClick={handleRemoveClick}>Remover</div>
+        </div>
       </div>
-      <div className="text-lg flex-grow-0">
-        <button onClick={decQty} className="bg-zinc-300 px-2 py-1 rounded-l-full">-</button>
-        <input className="bg-zinc-300 px-1 py-1 text-center remove-arrow w-10" type="number" disabled value={qty}></input>
-        <button onClick={addQty} className="bg-zinc-300 px-2 py-1 rounded-r-full">+</button>
+      <div className="text-lg flex-grow-0 flex flex-col justify-center items-center">
+        <div className="">
+          <button onClick={decQty} className="bg-zinc-300 px-2 py-1 rounded-l-full">-</button>
+          <input className="bg-zinc-300 px-1 py-1 text-center remove-arrow w-10" type="number" disabled value={qty}></input>
+          <button onClick={addQty} className="bg-zinc-300 px-2 py-1 rounded-r-full">+</button>
+        </div>
       </div>
-      <div>R${item.product.price.toFixed(2)}</div>
-      <div>R${(item.product.price * qty).toFixed(2)}</div>
+      <div className="text-center self-center">R${item.product.price.toFixed(2)}</div>
+      <div className="text-center self-center">R${(item.product.price * qty).toFixed(2)}</div>
     </Fragment>
   )
 };
