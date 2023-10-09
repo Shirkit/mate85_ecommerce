e626df6c3352e2ff109ebc0bdfd95a1dc00a917a
Shirkit <Shirkit@users.noreply.github.com>
2023-10-09 13:37:37 -0300

    fix(orders): :bug: Mostrado nome do comprador, moeda e status

--- app/admin/order/page.js
+++ app/admin/order/page.js
@@ -1,19 +1,44 @@
 import { AdminTable } from "@/components/admin/adminTable";
-import { queryAllOrders } from "./actions"
+import { prisma } from "@/utils/prisma";
 
-const orders = await queryAllOrders();
+export default async function ManageOrders() {
+    const orders = await prisma.order.findMany({
+        select: {
+            id: true,
+            status: true,
+            total: true,
+            user: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    })
 
-const actions = [
-    {
-        name: 'Edit',
-        color: 'blue',
-        dest: '/admin/order/edit/$1'
+    for (let i = 0; i < orders.length; i++) {
+        const element = orders[i];
+        element.user = element.user.name
+        element.total = "R$"+element.total.toFixed(2)
+        switch(element.status) {
+            case "completed": element.status = "Completado"; break
+            case "shipped": element.status = "Enviado"; break
+            case "payment-pending": element.status = "Pagamento Pendente"; break
+            case "processing": element.status = "Processando"; break
+            case "waiting": element.status = "Aguardando"; break
+            case "delivered": element.status = "Entregue"; break
+        }
     }
-]
 
-const headers = ['ID', 'User_ID', 'Status', 'Total', 'Action']
+    const actions = [
+        {
+            name: 'Edit',
+            color: 'blue',
+            dest: '/admin/order/$1'
+        }
+    ]
 
-export default function ManageOrders() {
+    const headers = ['Pedido', 'Status', 'Total', 'Comprador', 'Action']
+
     return (
         <div>
             <AdminTable
