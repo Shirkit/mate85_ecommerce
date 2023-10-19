705ac4c327722c0983f6e1ee09bfc9f9920cac54
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-10-19 16:02:11 -0300

    feat: refactor style in website

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -17,7 +17,9 @@ export default function RootLayout({ children }) {
 	return (
 		<AuthProvider>
 			<html lang="en">
-				<body className={inter.className}>
+				<body
+					className={`${inter.className} flex flex-col items-center justify-center`}
+				>
 					<CartProvider>
 						<Navbar />
 						<ToastContainer></ToastContainer>
