5701eed0dc70e5e669fddb47569a8b52a548d7b9
Shirkit <Shirkit@users.noreply.github.com>
2023-11-02 11:28:36 -0300

    fix(product): :bug: Eu deixei o código antigo ao invés de deiar o código novo

--- app/(website)/product/[id]/page.js
+++ app/(website)/product/[id]/page.js
@@ -16,35 +16,37 @@ export default async function Produto({ params, searchParams }) {
         }
     })
 
-	// ! Isso não está funcionando por algum motivo, quando passa pra segunda página, ele está repetindo alguns dos campos da primeira página
-	// TODO procurar entender porque isso acontece
+    // ! Isso não está funcionando por algum motivo, quando passa pra segunda página, ele está repetindo alguns dos campos da primeira página
+    // TODO procurar entender porque isso acontece
 
-	pages = Math.ceil(pages / take)
-	const produto = await prisma.product.findFirst({
-		where: {
-			id: parseInt(params.id),
-		},
-		include: {
-			reviews: {
-				take: take,
-				skip: (page - 1) * take,
-				orderBy: {
-					rating: 'desc',
-				},
-				include: {
-					user: {},
-				},
-			},
-			product_item: {},
-		},
-	})
+    pages = Math.ceil(pages / take)
+    const produto = await prisma.product.findFirst({
+        where: {
+            id: parseInt(params.id)
+        }, include: {
+            reviews: {
+                take: take,
+                skip: (page - 1) * take,
+                orderBy: [{
+                    rating: 'desc'
+                }, {
+                    id: 'desc'
+                }],
+                include: {
+                    user: {}
+                }
+            },
+            product_item: {}
+        }
+    })
 
-	let maxPrice = -1,
-		minPrice = Number.MAX_SAFE_INTEGER
-	produto.product_item.forEach((item) => {
-		maxPrice = Math.max(maxPrice, item.price)
-		minPrice = Math.min(minPrice, item.price)
-	})
+    // console.log(page)
+    // console.log(produto)
+    let maxPrice = -1, minPrice = Number.MAX_SAFE_INTEGER
+    produto.product_item.forEach(item => {
+        maxPrice = Math.max(maxPrice, item.price)
+        minPrice = Math.min(minPrice, item.price)
+    });
 
 	return (
 		<article className="max-w-7xl w-full py-16 px-8 flex flex-col gap-16">
