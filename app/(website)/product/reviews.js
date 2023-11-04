ea3c60edac513d3a14a941e56516cda424fb6e94
Shirkit <Shirkit@users.noreply.github.com>
2023-11-04 10:39:37 -0300

    feat(login): :sparkles: Review apenas para usuário logado
    E ainda verifica se o usuário fez a compra anteriormente

--- app/(website)/product/reviews.js
+++ app/(website)/product/reviews.js
@@ -1,11 +1,16 @@
 "use server"
 
+import { getServerSession } from "@/app/api/auth/[...nextauth]/route"
 import { prisma } from "@/utils/prisma"
 import { revalidatePath } from "next/cache"
 
 async function processReview(formData) {
     // TODO pegar o usuário atual
-    const currentUserId = (await prisma.user.findFirst({})).id
+    const currentUserId = (await prisma.user.findFirst({
+        where: {
+            id: (await getServerSession()).user.id
+        }
+    })).id
     const productsId = parseInt(formData.get('productId'))
     if (await canAddReview(currentUserId, productsId)) {
         await prisma.review.create({
@@ -19,7 +24,7 @@ async function processReview(formData) {
         })
         return revalidatePath(`/product/${productsId}`)
     }
-    return { message: 'Não autorizado!' }
+    return { message: 'Não autorizado! Você precisa ter comprado e recebido esse produto para fazer uma avaliação.' }
 }
 
 async function canAddReview(userId, productId) {
@@ -28,8 +33,8 @@ async function canAddReview(userId, productId) {
             users_id: userId,
             order_items: {
                 some: {
-                    products_id: {
-                        equals: productId
+                    product: {
+                        product_id: productId
                     }
                 }
             },
