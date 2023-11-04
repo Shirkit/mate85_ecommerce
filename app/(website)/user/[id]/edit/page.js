44e84f27219e586131451b5c1f4b2943b387cb6b
Shirkit <Shirkit@users.noreply.github.com>
2023-11-04 11:04:37 -0300

    fix(user-account): :sparkles: Tela de "/user" agora leva em conta o usuário logado

--- app/(website)/user/[id]/edit/page.js
+++ app/(website)/user/[id]/edit/page.js
@@ -1,12 +1,11 @@
 "use client"
-import React, { useEffect, useState, useTransition } from 'react';
-import { getUserById,updateUser } from './actions';
-import UserOrders from "@/app/(website)/user/[id]/edit/userOrders"
+import React, { useEffect, useState } from 'react';
+import { updateUser } from './actions';
+import UserOrders from "@/app/(website)/user/userOrders"
+import { useSession, signIn, signOut } from 'next-auth/react'
 
-const ProfileEditComponent = ({params}) => {
+const ProfileEditComponent = ({ params }) => {
   const [formData, setFormData] = useState({});
-  const [isPending,startTransition] = useTransition();
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData(prevFormData => ({
@@ -15,74 +14,91 @@ const ProfileEditComponent = ({params}) => {
     }));
   };
 
-   
+  const { data: session, status } = useSession()
+
   useEffect(() => {
-    if(!isPending){
-      startTransition(async () => {
-        const user = await getUserById(params.id);
-        setFormData(user)
-      })
+    if (session) {
+      setFormData(session.user)
     }
-  },[])
+  }, [session])
 
   const handleOnSave = (e) => {
     formData.id = params.id
     updateUser(formData);
     window.location.reload();
   }
-  
-  return (
-    <div className='flex my-10'>
-      <div className="max-w-md mx-auto bg-white rounded-lg p-8 border">
-      <h1 className="text-2xl mb-4 text-bold">Editar Conta</h1>
-    
-      <div className="mb-4">
-        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Nome:</label>
-        <input
-          type="text"
-          id="username"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="border rounded py-2 px-3 w-full focus:outline-none focus:border-blue-400"
-          required
-        />
+
+  if (status == "authenticated") {
+    return (
+      <div className='flex my-10 gap-4 max-w-7xl w-full'>
+        <div className="max-w-md mx-auto bg-white rounded-lg p-8 border">
+          <h1 className="text-2xl mb-4 text-bold">Editar Conta</h1>
+
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Nome:</label>
+            <input
+              type="text"
+              id="username"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="border rounded py-2 px-3 w-full focus:outline-none focus:border-blue-400"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="border rounded py-2 px-3 w-full focus:outline-none focus:border-blue-400"
+              required
+              disabled
+            />
+          </div>
+
+          {/* <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="border rounded py-2 px-3 w-full focus:outline-none focus:border-blue-400"
+            required
+          />
+        </div> */}
+
+          <div className='flex justify-between mt-8 gap-8'>
+            <button className="flex border rounded-lg w-max px-4 py-2 bg-black text-white border-black duration-300 hover:bg-transparent hover:text-black" onClick={handleOnSave} type="submit">Salvar</button>
+            <button className='flex border rounded-lg w-max px-4 py-2 bg-black text-white border-black duration-300 hover:bg-transparent hover:text-black' onClick={() => signOut()}>Fazer logout</button>
+          </div>
+
+        </div>
+        {/* //TODO ver o que vamos mostrar de um pedido */}
+        <div className="mx-auto bg-white rounded-lg p-8 border w-full">
+          <UserOrders userId={session.user.id} className="" />
+        </div>
       </div>
 
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="border rounded py-2 px-3 w-full focus:outline-none focus:border-blue-400"
-          required
-        />
+    );
+  } else if (status == "unauthenticated") {
+    return (
+      <div className='flex my-10'>
+        <div className="max-w-md mx-auto bg-white rounded-lg p-8 border flex flex-col items-center gap-4">
+          <p>Você precisa estar logado para ver seus pedidos</p>
+          <button onClick={() => signIn()} className='flex border rounded-lg w-max px-4 py-2 bg-black text-white border-black duration-300 hover:bg-transparent hover:text-black'>Fazer login</button>
+        </div>
       </div>
+    )
+  }
 
-      <div className="mb-4">
-        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Senha:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          className="border rounded py-2 px-3 w-full focus:outline-none focus:border-blue-400"
-          required
-        />
-      </div>
 
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick = {handleOnSave}>Salvar</button>
-
-      </div>
-      {/* //TODO ver o que vamos mostrar de um pedido */}
-      <UserOrders userId = {params.id}/> 
-    </div>
-   
-  );
 };
 
 export default ProfileEditComponent;
