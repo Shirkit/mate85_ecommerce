519ceb27b11fef46692ff79ec2ab50350016024e
Danilo Andrade <andrade20danilo@gmail.com>
2023-11-06 20:37:15 -0300

    fix: Card component com dois return's

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 import RenderStars, { renderStars } from './stars'
 
 export default async function Card(props) {
-	return (
-		<div className="flex flex-col justify-center gap-1 hover:opacity-70 transition-opacity">
-			<img
-				src={props.image}
-				alt="Product Photo"
-				className="w-full h-80 object-cover mb-2 rounded-lg"
-			/>
-			<h1 className="text-xl font-semibold mb-2">{props.name}</h1>
 
   return (
     <div className="bg-white rounded-lg shadow-lg p-4 m-4 flex flex-col items-center">
