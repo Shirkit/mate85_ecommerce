1507a95424cc5391368a310b6de6fbd4329e07cf
Shirkit <Shirkit@users.noreply.github.com>
2023-11-18 09:41:35 -0300

    fix(product): :lipstick: Quantidade máxima disponível
    Faltou a parte do produto para não deixar adicionar além da quantidade disponível além de renderizar a quantidade atual do estoque

--- components/ui/filtros.jsx
+++ components/ui/filtros.jsx
@@ -9,11 +9,15 @@ export default function Filtros({ produto }) {
 	const [sku, setSku] = useState()
 	const [price, setPrice] = useState()
     const [enable, setEnable] = useState()
+	const [stock, setStock] = useState()
 
 	useEffect(() => {
 		for (let i = 0; i < produto.product_item.length; i++) {
-			if (produto.product_item[i].sku == sku)
+			if (produto.product_item[i].sku == sku) {
 				setPrice(produto.product_item[i].price)
+				setStock(produto.product_item[i].amount)
+				setQty(Math.min(qty, produto.product_item[i].amount))
+			}
 		}
 	}, [sku])
 
@@ -26,7 +30,7 @@ export default function Filtros({ produto }) {
 	const [qty, setQty] = useState(1)
 
 	function addQty() {
-		setQty(qty + 1)
+		setQty(Math.min(qty + 1, stock))
 	}
 
 	function decQty() {
@@ -59,9 +63,11 @@ export default function Filtros({ produto }) {
 				</ToggleGroup.Root>
 			</div>
 			{price && (
-				<span className="text-2xl">
+				<><span className="text-2xl">
 					Preço: R${(price * qty).toFixed(2)}
 				</span>
+				<span className='text-xs'>Estoque: {stock > 0 ? stock : "indisponível"} {stock > 1? "unidades disponíveis" : (stock  == 1 ? "unidade disponível" : "")}</span>
+				</>
 			)}
 			<hr className="my-4"></hr>
 			<div className="flex flex-row text-black gap-4">
@@ -85,7 +91,7 @@ export default function Filtros({ produto }) {
 						+
 					</button>
 				</div>
-				<button onClick={handleClick} disabled={enable ? true : false} className="border-2 border-black bg-black text-white rounded-full hover:bg-transparent hover:text-black duration-300 w-auto flex-grow py-2 px-8">
+				<button onClick={handleClick} disabled={enable ? true : false} className="border-2 border-black bg-black text-white rounded-full hover:bg-transparent hover:text-black duration-300 w-auto flex-grow py-2 px-8 disabled:text-black disabled:border-zinc-300 disabled:bg-zinc-300">
                     {enable && (
                         <>
                             Tamanho não disponível
