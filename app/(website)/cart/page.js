b0424e4809d5051977ae204f09e125a1008cb524
Shirkit <Shirkit@users.noreply.github.com>
2023-11-18 09:36:35 -0300

    fix(cart): :lipstick: Corrigido gap no carrinho, quantidade máxima e remover
    Movido o botão remover para em baixo da quantidade; só deixa aumentar a quantidade máxima até o máximo de unidades disponíveis

--- app/(website)/cart/page.js
+++ app/(website)/cart/page.js
@@ -8,7 +8,7 @@ export default function Cart() {
 	const { cartItems, cartTotal } = useCart()
 
 	return (
-		<div className="max-w-7xl w-full py-5 px-8 grid grid-cols-5 gap-8">
+		<div className="max-w-7xl w-full pt-5 pb-8 px-8 grid grid-cols-5 gap-8">
 			<h1 className="mb-2 text-4xl font-black col-span-5">Carrinho</h1>
 			{cartItems.length === 0 ? (
 				<h2>Seu carrinho está vazio</h2>
@@ -43,7 +43,7 @@ export default function Cart() {
 						<span>R${cartTotal.toFixed(2)}</span>
 					</div>
 					<span className="col-span-3"></span>
-					<div className="col-span-2 bg-zinc-50 p-5">
+					<div className="[margin-top:-2rem] col-span-2 bg-zinc-50 p-5">
 						<Link href="/checkout" className="col-span-2">
 							<button className="bg-black text-white border-2 border-black hover:bg-transparent hover:text-black duration-300 w-full rounded-full py-2 px-4">
 								Finalizar compra
